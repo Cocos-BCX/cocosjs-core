10e51be87670f1e188ecb024e86d2903ed40d83c
zhen.liu <zhen.liu@chukong-inc.com>
2019-05-28 14:15:30 +0800

    min

--- packages/plugin-bcx/src/index.js
+++ packages/plugin-bcx/src/index.js
@@ -1,83 +1,82 @@
 import {
-    Plugin,
-    SocketService,
-    WALLET_METHODS,
-    Blockchains,
-    PluginTypes
+  Plugin,
+  SocketService,
+  WALLET_METHODS,
+  Blockchains,
+  PluginTypes
 } from 'cocosjs-core';
 import Cocosjs from 'cocosjs-core'
 const proxy = (dummy, handler) => new Proxy(dummy, handler);
-
 let socketService = SocketService;
 export default class CocosBcx extends Plugin {
-
-    constructor() {
-        super(Blockchains.COCOSBCX, PluginTypes.BLOCKCHAIN_SUPPORT)
-    }
-
-    setSocketService(_s) {
-        socketService = _s;
-    }
-
-    hookProvider() {
-        throw new Error('cocos hook provider not enabled yet.');
-    }
-
-    signatureProvider(...args) {
-        const throwIfNoIdentity = args[0];
-        return (_bcx) => {
-            // throwIfNoIdentity(
-            return proxy(_bcx, {
-                get(instance, method) {
-                    if (typeof instance[method] === 'function') return (...args) => {
-                        if (Cocosjs.cocos.isExtension) return instance[method](...args)
-                        if (method === WALLET_METHODS.transferAsset) {
-                            // return new Promise(resolve => {
-                            //     resolve(CocosBcx.methods()[WALLET_METHODS.transferAsset](...args))
-                            // })
-                            return new Promise((resolve, reject) => {
-                                CocosBcx.methods()[WALLET_METHODS.transferAsset](...args).then(res => {
-                                    return resolve(res)
-                                }).catch(err => {
-                                    return reject(err)
-                                })
-                            })
-                        }
-                        if (method === WALLET_METHODS.callContractFunction) {
-                            // return new Promise(resolve => {
-                            //     resolve(CocosBcx.methods()[WALLET_METHODS.callContractFunction](...args))
-                            // })
-                            return new Promise((resolve, reject) => {
-                                CocosBcx.methods()[WALLET_METHODS.callContractFunction](...args).then(res => {
-                                    return resolve(res)
-                                }).catch(err => {
-                                    return reject(err)
-                                })
-                            })
-                        }
-                        return instance[method](...args)
-                    };
-                    else return instance[method];
-
-                }
-            });
+  constructor() {
+    super(Blockchains.COCOSBCX, PluginTypes.BLOCKCHAIN_SUPPORT)
+  }
+  setSocketService(_s) {
+    socketService = _s;
+  }
+  hookProvider() {
+    throw new Error('cocos hook provider not enabled yet.');
+  }
+  signatureProvider(...args) {
+    const throwIfNoIdentity = args[0];
+    return (_bcx) => {
+      // throwIfNoIdentity(
+      return proxy(_bcx, {
+        get(instance, method) {
+          if (typeof instance[method] === 'function') return (...args) => {
+            if (Cocosjs.cocos.isExtension) return instance[method](...args)
+            if (method === WALLET_METHODS.transferAsset) {
+              return new Promise((resolve, reject) => {
+                CocosBcx.methods()[WALLET_METHODS.transferAsset](...args).then(res => {
+                  return resolve(res)
+                }).catch(err => {
+                  return reject(err)
+                })
+              })
+            }
+            if (method === WALLET_METHODS.callContractFunction) {
+              return new Promise((resolve, reject) => {
+                CocosBcx.methods()[WALLET_METHODS.callContractFunction](...args).then(res => {
+                  return resolve(res)
+                }).catch(err => {
+                  return reject(err)
+                })
+              })
+            }
+            if (method === WALLET_METHODS.getAccountInfo) {
+              return new Promise((resolve, reject) => {
+                CocosBcx.methods()[WALLET_METHODS.getAccountInfo]({}).then(res => {
+                  return resolve(res)
+                }).catch(err => {
+                  return reject(err)
+                })
+              })
+            }
+            return instance[method](...args)
+          };
+          else return instance[method];
         }
+      });
     }
-
-    static methods() {
-        return {
-            [WALLET_METHODS.transferAsset]: (args) => socketService.sendApiRequest({
-                type: 'requestTransfer',
-                payload: args
-            }),
-            [WALLET_METHODS.callContractFunction]: (args) => socketService.sendApiRequest({
-                type: 'callContractFunction',
-                payload: args
-            })
-        }
+  }
+  static methods() {
+    return {
+      [WALLET_METHODS.transferAsset]: (args) => socketService.sendApiRequest({
+        type: 'requestTransfer',
+        payload: args
+      }),
+      [WALLET_METHODS.callContractFunction]: (args) => socketService.sendApiRequest({
+        type: 'callContractFunction',
+        payload: args
+      }),
+      [WALLET_METHODS.getAccountInfo]: (args) => socketService.sendApiRequest({
+        type: 'getAccountInfo',
+        payload: {}
+      }),
     }
+  }
 }
-
 if (typeof window !== 'undefined') {
-    window.CocosBcx = CocosBcx;
+  window.CocosBCX = CocosBcx;
 }
---
+++ packages/plugin-bcx/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+const webpack = require('webpack')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'plugin.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // module: {
+  //   rules: [{
+  //     test: /\.m?js$/,
+  //     exclude: /(node_modules|bower_components)/,
+  //     use: {
+  //       loader: 'babel-loader',
+  //       options: {
+  //         presets: ['@babel/preset-env']
+  //       }
+  //     }
+  //   }]
+  // }
+}
